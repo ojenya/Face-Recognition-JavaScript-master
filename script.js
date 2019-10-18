a335fb8192ea0a7c23c4aa707558966aaeec3afe
Jenya <jeenjenya@gmail.com>
2019-10-19 01:05:33 +0800

    6

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'j3nqa']
+    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'j3nqa', 'Jim Rhodes', 'Thor', 'Tony Stark']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
