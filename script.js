2948858d1ca13a5551f193ab073f11eda4f9c63c
Jenya <jeenjenya@gmail.com>
2019-10-19 11:05:42 +0800

    9
    9

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function loadLabeledImages() {
         labels.map(async label => {
             const descriptions = []
             for (let i = 1; i <= 2; i++) {
-                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`https://ojenya.github.io/Face-Recognition-JavaScript-master/labeled_images/${label}/${i}.jpg`)
                     //const img = await faceapi.fetchImage()
                     //const img = await faceapi.nets.ssdMobilenetv1.loadFromDisk(`__dirname/label_images/${label}/${i}.jpg`);
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
