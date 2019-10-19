22776977ff53a69f3e7670fd58cb2364566fe02d
Jenya <jeenjenya@gmail.com>
2019-10-19 10:04:01 +0800

    7

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function loadLabeledImages() {
             const descriptions = []
             for (let i = 1; i <= 2; i++) {
                 const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+                    //const img = await faceapi.fetchImage(`__dirname/label_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 descriptions.push(detections.descriptor)
             }
